6af6a74697ff259edad26bcc64ca7d1e7d6c784b
pranayshinde855 <pranayshinde855@gmail.com>
2022-07-03 12:52:15 +0530

    Armstrong Number Between Two Intervals

---
+++ Arm Strong number between two numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Armstrong_Number_Between_Two_Intervals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, r, n, sum;
+            Console.WriteLine("Enter two numbers");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = num1; i <= num2; i++)
+            {
+
+                r = 0;
+                sum = 0;
+                n = i;
+                while (n!=0)
+                {
+                    r = n % 10;
+                    sum = sum + (r * r * r);
+                    n = n / 10;
+                }
+                if (i == sum)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
