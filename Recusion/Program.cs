1d3742426d56e0e658c15ad0c27dd626feea29cf
pranayshinde855 <pranayshinde855@gmail.com>
2022-07-09 16:21:27 +0530

    Recusrsion Program

---
+++ Recusion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recusion
+{
+    class Program
+    {
+        static int Print_Recursion_Values(int static_Val, int toPrint_Val)
+        {
+            if(toPrint_Val < 1)
+            {
+                return static_Val;
+            }
+            toPrint_Val--;
+            Console.Write(static_Val);
+            return Print_Recursion_Values(static_Val + 1, toPrint_Val);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n Recursion : Print the first n natural number :\n");
+            Console.Write("\n");
+            Console.Write(" How many numbers to print : ");
+            int ctr = Convert.ToInt32(Console.ReadLine());
+
+            Print_Recursion_Values(1, ctr);
+        }
+    }
+}
