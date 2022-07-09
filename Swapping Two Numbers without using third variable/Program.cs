d107663dda40e0ec52817580cbc9dccae9fe093f
pranayshinde855 <pranayshinde855@gmail.com>
2022-07-09 15:44:38 +0530

    Swapping Two Numbers without using third variable

---
+++ Swapping Two Numbers without using third variable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Swapping_Two_Numbers_without_using_third_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number.");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number.");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            num1 = num1 * num2;
+            num2 = num1/num2;
+            num1 = num1 / num2;
+
+            Console.WriteLine("After swapping First number." + num1);
+            Console.WriteLine("After swapping Second number." + num2);
+        }
+    }
+}
